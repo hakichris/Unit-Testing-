fab855ff2c3e75e6cb4593350f7c18fc2bb39e83
Chris <hakizachrist1@gmail.com>
2023-02-02 09:17:56 +0200

    Create test2

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 const sum = require('./sum');
 const stringLength = require('./stringlength');
+const calculator = require('./test2');
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
 
 test('count characters in a string', () => {
   expect(stringLength("hakizimanana")).toBeGreaterThan(1) && toBeLessThan(10);
+});
+
+test('adding numbers', () => {
+  expect( calculator.add(1,2)).toBe(3);
 });
---
+++ test2.js
@@ -0,0 +1,21 @@
+class calculator {
+
+   static add = (a,b) => {
+    return a+b
+   }
+
+   static subs = (a,b) => {
+    return a-b
+   }
+
+   static multiply = (a,b) => {
+    return a*b
+   }
+
+   static divide = (a,b) => {
+    return a/b
+   }
+
+}
+
+module.exports = calculator;
